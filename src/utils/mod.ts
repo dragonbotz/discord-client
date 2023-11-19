e8a6cd3a095ee31d07640fc30370541d3aa2d420
Lahène Belhadi <lahcene.belhadi@gmail.com>
2023-11-19 17:14:47 +0100

    fix: Adds getClientId()

--- src/utils/mod.ts
+++ src/utils/mod.ts
@@ -9,8 +9,17 @@
 /**
  * Retrieves the client's discord token
  *
- * @returns
+ * @returns {string | undefined} - The token or `undefined` if not found
  */
 export function getDiscordToken(): string | undefined {
 	return Deno.env.get("DISCORD_TOKEN");
+}
+
+/**
+ * Retrieves the client's id
+ *
+ * @returns {string | undefined} - The client id or `undefined` if not found
+ */
+export function getClientId(): string | undefined {
+	return Deno.env.get("CLIENT_ID");
 }
