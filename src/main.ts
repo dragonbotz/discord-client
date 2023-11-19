346be1b21f85678744e479f53fd23655512df0ac
Lahène Belhadi <lahcene.belhadi@gmail.com>
2023-11-19 17:15:43 +0100

    fix: Adds slash command register

--- src/main.ts
+++ src/main.ts
@@ -7,8 +7,12 @@
  * @author Lahcène Belhadi <lahcene.belhadi@gmail.com>
  */
 import { load } from "https://deno.land/std@0.207.0/dotenv/mod.ts";
-import { Client, Events, GatewayIntentBits } from "npm:discord.js@14.14";
-import { getDiscordToken } from "./utils/mod.ts";
+import { Client, Events, GatewayIntentBits, REST, Routes, SlashCommandBuilder } from "npm:discord.js@14.14";
+import { getDiscordToken, getClientId } from "./utils/mod.ts";
+
+// commands
+import { Command } from "./core/command.ts";
+import { HelloCommand } from "./commands/hello.ts";
 
 /**
  * Dragon Bot Z's Discord client entry point
@@ -23,10 +27,52 @@ async function main(): Promise<void> {
 		Deno.exit(1);
 	}
 
+	const clientId = getClientId();
+	if (clientId == undefined) {
+		console.error("Client ID is undefined");
+		Deno.exit(1);
+	}
+
 	// discord client
 	const client: Client = new Client({intents: [GatewayIntentBits.Guilds]});
+
+	// Client events
+	// on client ready
 	client.once(Events.ClientReady, client => {
 		console.info(`Connected as ${client.user.tag}`);
+	});
+
+	// load commands
+	const commands = new Map<string, Command>()
+		.set("hello", new HelloCommand());
+
+	// puts the commands in an array to pass to discord
+	const commandsRegister = [];
+	for (let key of commands.keys()) {
+		commandsRegister.push(commands.get(key).getData().toJSON());
+	}
+
+	// send commands to discord
+	const rest = new REST().setToken(discordToken);
+	await rest.put(Routes.applicationGuildCommands(clientId, "850778288473243648"), {body: commandsRegister});
+
+	// on interaction
+	client.on(Events.InteractionCreate, async interaction => {
+		if (!interaction.isChatInputCommand()) {
+			return;
+		}
+
+		const command = commands.get(interaction.commandName);
+		if (command == undefined) {
+			console.error(`Command not found: ${interaction.commandName}`);
+			return;
+		}
+
+		try {
+			await command.execute(interaction);
+		} catch (error) {
+			console.error(`${error}`);
+		}
 	});
 
 	client.login(discordToken);
